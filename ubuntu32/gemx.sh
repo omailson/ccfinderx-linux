d3648dd746e2a3cdd8ded2f5dc1885df975254fc
Mailson Lira <mailson@gmail.com>
2011-08-13 11:58:44 -0300

    Load GUI

--- ubuntu32/gemx.sh
+++ ubuntu32/gemx.sh
@@ -2,6 +2,6 @@
 
 pushd `dirname $0`
 
-java -Djava.library.path=/usr/lib/jni/:. -classpath /usr/lib/java/swt-gtk-3.5.jar:icu4j-4_0_1.jar:icu4j-charsets-4_0_1.jar:icu4j-localespi-4_0_1.jar:pathjson.jar:/usr/lib/java/trove-2.0.4.jar:.:gemxlib.jar GemXMain
+java -Djava.library.path=/usr/lib/:. -classpath /usr/share/java/swt.jar:icu4j-4_0_1.jar:icu4j-charsets-4_0_1.jar:icu4j-localespi-4_0_1.jar:pathjson.jar:trove-2.0.4.jar:gemxlib.jar:. GemXMain
 
 popd
